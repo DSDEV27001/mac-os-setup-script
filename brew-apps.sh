bdd38d730c40b0c4637af7b7f9d263d60111eebd
DSDEV27001 <52406362+DSDEV27001@users.noreply.github.com>
2020-06-29 16:35:26 +0100

    Added missing files

---
+++ brew-apps.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Install apps
+
+brew tap homebrew/completions
+brew install homebrew/completions/docker-completion
+brew install homebrew/completions/docker-compose-completion
+
+brew tap caskroom/cask
+brew cash install little-snitch
+brew cask install atom
+# brew cask install dashlane
+# brew cask install data-rescue
+brew cask install docker
+# brew cask install dropbox
+# brew cask install evernote
+brew cask install figma
+# brew cask install firefox
+brew cask install github-desktop
+brew cask install brave-browser
+# brew cask install google-drive
+brew cask install iterm2
+# brew cask install itsycal
+brew cask install kindle
+brew cask install monolingual
+# modern postgressql client
+# brew cask install postico
+# brew cask install recordit
+brew cask install sequel-pro
+# brew cask install sketch
+# brew cask install sketch-toolbox
+brew cask install sky-fonts
+# brew cask install skype
+# brew cask install slack
+brew cask install soulver
+brew cask install spotify
+brew cask install transmit
+brew cask install vlc
+brew cask install wifi-explorer
+brew cask install zoomus
+brew cask install microsoft-office
+brew cask install pycharm
+brew cask install whatsapp
+brew cash install now-tv-player
+
+#tap caskroom/fonts
+#cask font-inconsolata
+#cask font-fira-mono
