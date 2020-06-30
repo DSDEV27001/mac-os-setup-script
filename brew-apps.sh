2b5809a0bcb86d8a59880ae34cec60ad050bcb30
DSDEV27001 <52406362+DSDEV27001@users.noreply.github.com>
2020-06-30 14:43:28 +0100

    Update brew-apps.sh

--- brew-apps.sh
+++ brew-apps.sh
@@ -2,26 +2,12 @@
 
 # Install apps
 
-# Deprecated
-# brew tap homebrew/completions
-# brew install homebrew/completions/docker-completion
-# brew install homebrew/completions/docker-compose-completion
-
-# brew tap homebrew/cask-cask
 brew cask install little-snitch
 brew cask install atom
-# brew cask install dashlane
-# brew cask install data-rescue
 brew cask install docker
-# brew cask install dropbox
-# brew cask install evernote
 brew cask install figma
-# brew cask install firefox
-# brew cask install github-desktop
 brew cask install brave-browser
-# brew cask install google-drive
 brew cask install iterm2
-# brew cask install itsycal
 brew cask install kindle
 brew cask install monolingual
 # modern postgressql client
@@ -31,10 +17,6 @@ brew cask install sequel-pro
 # brew cask install sketch
 # brew cask install sketch-toolbox
 brew cask install skyfonts
-# brew cask install skype
-# brew cask install slack
-# brew cask install soulver
-# brew cask install spotify
 brew cask install transmit
 brew cask install vlc
 brew cask install wifi-explorer
@@ -43,7 +25,3 @@ brew cask install microsoft-office
 brew cask install pycharm
 brew cask install whatsapp
 brew cask install now-tv-player
-
-#tap caskroom/fonts
-#cask font-inconsolata
-#cask font-fira-mono
